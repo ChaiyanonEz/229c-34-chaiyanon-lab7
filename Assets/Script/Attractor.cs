71889826905b5a41e4b015b6b3ba8bcd8c51dc8e
Chaiyanon3010 <chaiyanon.prom@bumail.net>
2024-03-12 10:30:22 +0700

    Update
    คอมค้าง แล้วอัพเดทอันใหม่ไม่ได้ ต้องCommitใหม่หมด

--- Assets/Script/Attractor.cs
+++ Assets/Script/Attractor.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Attractor : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    private float G = 6.674f; //F = G(m1*m2)/r^2
 
     void Attract(Attractor other)
     {
         Rigidbody rb2 = other.rb;
+        
+        //F = G(m1*m2)/r^2
+        Vector3 direction = rb.position - rb2.position;
+        
+        //Find Distance beteen 2 objects from Vector3
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G*(rb.mass * rb2.mass)/Mathf.Pow(distance,2);
+
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+        
+        rb.AddForce(finalForce);
+
     }
 
 
-}
+}//end
